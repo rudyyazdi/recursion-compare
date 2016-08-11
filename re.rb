8bc47bbdb2a468a5972ac802576ae23434804c8f
rudyyazdi <rudyyazdi@gmail.com>
2016-08-11 10:22:37 +1000

    cleanup

--- re.rb
+++ re.rb
@@ -1,7 +1,7 @@
-def coin_com(amount, array)
-  return 0 if amount < 0 || array.size == 0
+def coin(amount, array)
+  return 0 if amount < 0 || array.empty?
   return 1 if amount == 0
-  coin_com(amount-array[0], array) + coin_com(amount, array[1..array.size-1])
+  coin(amount - array[0], array) + coin(amount, array[1..array.size - 1])
 end
 
-p coin_com(1000, [1,2,5])
+p coin(1000, [1, 2, 5])
